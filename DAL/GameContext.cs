1737ff377696c0550cba03a498d4408861376ada
mmarie4 <max.marie.4@gmail.com>
2020-11-15 16:13:49 +0100

    API design

---
+++ DAL/GameContext.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class GameContext : DbContext
+    {
+        public GameContext()
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Score> Scores { get; set; }
+    }
+}
