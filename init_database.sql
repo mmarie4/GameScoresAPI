d65d1e84a221ab470928a9c5c1bcf13d5865e0aa
mmarie4 <max.marie.4@gmail.com>
2021-01-27 19:34:24 +0100

    init databse script

---
+++ init_database.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE players {
+  device_id varchar
+  name varchar
+  country varchar
+}
+
+CREATE TABLE scores {
+  id uuid
+  device_id varchar
+  distance int
+  duration int
+  coins_collected int
+  created_at timestamp
+}
