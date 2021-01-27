74f4d24f021267756dbd28e1366e43845f96ad7d
mmarie4 <max.marie.4@gmail.com>
2021-01-27 19:35:58 +0100

    init database script

--- init_database.sql
+++ init_database.sql
@@ -1,14 +1,14 @@
-﻿CREATE TABLE players {
-  device_id varchar
-  name varchar
+﻿CREATE TABLE public.players (
+  device_id varchar,
+  "name" varchar,
   country varchar
-}
+);
 
-CREATE TABLE scores {
-  id uuid
-  device_id varchar
-  distance int
-  duration int
-  coins_collected int
+CREATE TABLE public.scores (
+  id uuid,
+  device_id varchar,
+  distance int,
+  duration int,
+  coins_collected int,
   created_at timestamp
-}
+)
